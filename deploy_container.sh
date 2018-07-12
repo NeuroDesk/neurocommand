c72bbac5dc3333e5340ef34bc91521d39a55aef3
Steffen Bollmann <steffen.bollmann@cai.uq.edu.au>
2018-07-12 21:19:23 +1000

    trying to get the ON OFF scripts workign

--- deploy_container.sh
+++ deploy_container.sh
@@ -10,6 +10,7 @@ container=minc-toolkit-1.9.16.simg
 container_pull=shub://vfonov/minc-toolkit-containers:1.9.16
 
 container=minc_1p9p16_20180712.simg
+container=fsl_5p0p11_test.simg
 
 container_pull="scp steffen@203.101.224.252:/qrisvolume/caid/$container $container"
 
@@ -30,13 +31,14 @@ singularity exec --pwd $deploy_path $container ./binaryFinder.sh
 
 echo "create singularity executable for each regular executable in commands.txt"
 # $@ parses command line options.
-#test   executable="register"
-for executable in `cat commands.txt`; do \
+#test   
+executable="fslmath"
+#for executable in `cat commands.txt`; do \
    echo $executable > $PWD/${executable}; \
    echo "export PWD=\`pwd -P\`" > $executable 
    echo "singularity exec --pwd \$PWD --bind /gpfs1:/gpfs1 $deploy_path/$container $executable \$@" >> $executable
    chmod a+x $executable
-done
+#done
 
 echo "add $container to your PATH"
 export PWD=`pwd -P`
@@ -44,3 +46,15 @@ export PATH="$PATH:$PWD"
 echo "" >> ~/.bashrc
 echo "# $container in $PWD" >> ~/.bashrc
 echo "export PATH="\$PATH:$PWD"" >> ~/.bashrc
+
+#There will also be a problem with multiple containers and different versions. We should set up an executable script with each container that turns the executables on and off easily.-
+#something like:-
+touch TURNOFF_${container}
+touch TURNON_${container}
+echo "PATH=`echo $PATH | sed -e 's/:$PWD/$//'`">> TURNOFF_${container}
+echo "bash">>TURNOFF_${container}
+echo "sed -e 's/:\$PWD\/$//' ~/.bashrc">> TURNOFF_${container}
+echo "export PATH="$PWD:$PATH"">>/path/to/simg/TURNON_${container}
+echo "export PATH="$PWD:$PATH"">>~/.bashrc
+echo "bash">>TURNON_${container}
+
