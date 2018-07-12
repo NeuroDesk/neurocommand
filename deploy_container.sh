b602492646705c4f2926709aa4af5c10fdf16e7b
Steffen Bollmann <steffen.bollmann@cai.uq.edu.au>
2018-07-12 18:54:53 +1000

    removed sudo and tried to implement deploy path

--- deploy_container.sh
+++ deploy_container.sh
@@ -4,6 +4,10 @@
 #11/07/2018
 #SB & TS
 
+#Parameters
+container=minc-toolkit-1.9.16.simg
+container_web_address=shub://vfonov/minc-toolkit-containers:1.9.16
+
 #Check if singularity is installed, if not ask for module load command
 
 qq=`which  singularity`
@@ -15,14 +19,16 @@ qq=`which  singularity`
 	#apt-get singularity??? #FIXME
     fi
 
-container=minc-toolkit-1.9.16.simg
-container_web_address=shub://vfonov/minc-toolkit-containers:1.9.16
 
-#pull image
-#singularity pull --name $container $container_web_address
+#pull image if not yet there
+qq=`ls $container`
+if  [[  ${#qq} -lt 1 ]]; then
+   echo "pulling image"
+   singularity pull --name $container $container_web_address
+fi
 
 #figure out which executables exist
-sudo singularity shell --bind $PWD:/mnt/ --pwd /mnt/ $container
+singularity shell --bind $PWD:/mnt/ --pwd /mnt/ $container
 
 IFS=':'; \
 for i in $PATH; \
@@ -34,27 +40,27 @@ do test -d "$i" && find "$i" -maxdepth 1 -executable -type f -exec basename {} \
 # $@ parses command line options.
  
 for executable in `cat commands.txt`; do \
-   echo $executable > /path/to/singularity_exacutables/${executable}; \
-   echo "singularity exec --bind $PWD:/mnt/ --pwd /mnt/ $container /path/to/singularity_executables/$executable '$@'" > /path/to/singularity_executables/$executable
+   echo $executable > $PWD/${executable}; \
+   echo "singularity exec --bind $PWD:/mnt/ --pwd /mnt/ $PWD/$container $executable '$@'" > $executable
    chmod a+x $executable
 
 done
 
 #add /path/to/singularity_executables to your $PATH 
-export PATH="/path/to/singularity_executables/:$PATH"
-echo "export PATH="/path/to/singularity_executables/:$PATH"" >> ~/.bashrc
+export PATH="$PWD:$PATH"
+echo "# $container"
+echo "export PATH="$PWD:$PATH"" >> ~/.bashrc
 #Steffen FIXME figure out what order this needs to be appended.
-#FIXME also need a solution for where we store the simg executables i.e. /path/to/singularity_executables. Perhaps with the container? e.g., mkdir ./simg_executables
 
 
 #There will also be a problem with multiple containers and different versions. We should set up an executable script with each container
 #that turns the executables on and off easily. 
 #something like: 
-touch /path/to/singularity_executables/TURNOFF_${container}
-touch /path/to/singularity_executables/TURNON_${container}
-echo "PATH=`echo $PATH | sed -e 's/:\/path\/to\/simg\/$//'`">>/path/to/simg/TURNOFF_${container}
-echo "bash">>/path/to/simg/TURNOFF_${container}
-echo "sed -e 's/:\/path\/to\/simg\/$//' ~/.bashrc">>/path/to/simg/TURNOFF_${container}
-echo "export PATH="/path/to/singularity_executables/:$PATH"">>/path/to/simg/TURNON_${container}
-echo "export PATH="/path/to/singularity_executables/:$PATH"">>~/.bashrc
-echo "bash">>/path/to/simg/TURNON_${container}
+#touch /path/to/singularity_executables/TURNOFF_${container}
+#touch /path/to/singularity_executables/TURNON_${container}
+#echo "PATH=`echo $PATH | sed -e 's/:\/path\/to\/simg\/$//'`">>/path/to/simg/TURNOFF_${container}
+#echo "bash">>/path/to/simg/TURNOFF_${container}
+#echo "sed -e 's/:\/path\/to\/simg\/$//' ~/.bashrc">>/path/to/simg/TURNOFF_${container}
+#echo "export PATH="/path/to/singularity_executables/:$PATH"">>/path/to/simg/TURNON_${container}
+#echo "export PATH="/path/to/singularity_executables/:$PATH"">>~/.bashrc
+#echo "bash">>/path/to/simg/TURNON_${container}
