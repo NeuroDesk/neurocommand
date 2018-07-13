9d32183a21c1eb3909e8623bbdff08b9fecda123
Steffen Bollmann <steffen.bollmann@cai.uq.edu.au>
2018-07-13 14:37:12 +1000

    corrected naming

--- deploy_container.sh
+++ deploy_container.sh
@@ -5,44 +5,63 @@
 #11/07/2018
 #SB & TS
 
+
 #Parameters
 container=minc-toolkit-1.9.16.simg
 container_pull=shub://vfonov/minc-toolkit-containers:1.9.16
 
 container=minc_1p9p16_20180712.simg
-container=fsl_5p0p11_test.simg
-
+container=fsl_5p0p11_20180712.simg
 container_pull="scp steffen@203.101.224.252:/qrisvolume/caid/$container $container"
+
+
+echo "checking for singularity ..."
+qq=`which  singularity`
+if [[  ${#qq} -lt 1 ]]; then
+   echo "This requires singularity on your path. E.g. add "module load singularity/2.4.2" to your .bashrc"
+fi
+
+
 
 deploy_path=`pwd -P`
 echo "deploying in $deploy_path"
 echo "checking if container needs to be downloaded"
-#pull image if not yet there
 qq=`ls $container`
 if  [[  ${#qq} -lt 1 ]]; then
    echo "pulling image"
    $container_pull
 fi
 
-echo "checking out which executables exist inside container"
 
 
+echo "checking which executables exist inside container"
 singularity exec --pwd $deploy_path $container ./binaryFinder.sh
+
+
 
 echo "create singularity executable for each regular executable in commands.txt"
 # $@ parses command line options.
-#test   executable="fslmaths"
-for executable in `cat commands.txt`; do \
+#test   
+executable="fslmaths"
+#for executable in `cat commands.txt`; do \
    echo $executable > $PWD/${executable}; \
    echo "export PWD=\`pwd -P\`" > $executable 
    echo "singularity exec --pwd \$PWD --bind /gpfs1:/gpfs1 $deploy_path/$container $executable \$@" >> $executable
    chmod a+x $executable
-done
+#done
 
-echo "add $container to your PATH"
-export PWD=`pwd -P`
-export PATH="$PATH:$PWD"
-echo "" >> ~/.bashrc
-echo "# $container in $PWD" >> ~/.bashrc
-echo "export PATH="\$PATH:$PWD"" >> ~/.bashrc
+echo -e 'export PWD=`pwd -P`' > activate_${container}
+echo -e 'export PATH="$PATH:$PWD"' >> activate_${container}
+echo -e 'echo " " >> ~/.bashrc' >> activate_${container}
+echo -e 'echo "# Container in $PWD" >> ~/.bashrc' >> activate_${container}
+echo -e 'echo "export PATH="\$PATH:$PWD"" >> ~/.bashrc' >> activate_${container}
+
+echo "removing container first, in case it is a reinstall"
+cp deactivate_ deactivate_${container}
+chmod a+x deactivate_${container}
+source deactivate_${container} $deploy_path
+
+echo "adding $container to your PATH"
+chmod a+x activate_${container}
+source activate_${container}
 
