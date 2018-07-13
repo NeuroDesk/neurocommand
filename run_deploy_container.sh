35835fd03a6c786e16666aea4fee0cf1dc036948
Steffen Bollmann <steffen.bollmann@cai.uq.edu.au>
2018-07-13 20:48:03 +1000

    chaned to while loop - more robust to line breaks

--- run_deploy_container.sh
+++ run_deploy_container.sh
@@ -52,12 +52,12 @@ awk 'FNR==NR {a[$0]++; next} !a[$0]' host_commands.txt container_commands.txt > 
 echo "create singularity executable for each regular executable in commands.txt"
 # $@ parses command line options.
 #test   executable="fslmaths"
-for executable in `cat commands.txt`; do \
+while read executable; do \
    echo $executable > $PWD/${executable}; \
    echo "export PWD=\`pwd -P\`" > $executable 
    echo "singularity exec --pwd \$PWD --bind /gpfs1:/gpfs1 $deploy_path/$container $executable \$@" >> $executable
    chmod a+x $executable
-done
+done <commands.txt
 
 echo -e 'export PWD=`pwd -P`' > activate_${container}.sh
 echo -e 'export PATH="$PATH:$PWD"' >> activate_${container}.sh
