2c47cd3293b778f3bfe9765d2f3a53b22c6eb3a7
Steffen <steffen.bollmann@cai.uq.edu.au>
2020-04-22 21:09:50 +1000

    added module load of singularity

--- run_transparent_singularity.sh
+++ run_transparent_singularity.sh
@@ -95,7 +95,12 @@ qq=`which  singularity`
 if [[  ${#qq} -lt 1 ]]; then
    echo "This script requires singularity on your path. E.g. add module load singularity/2.4.2 to your .bashrc"
    echo "If you are root try again as normal user"
-   exit
+   echo "trying to module load:"
+   module load singularity/3.5.0
+   qq=`which  singularity`
+   if [[  ${#qq} -lt 1 ]]; then
+      exit
+   fi
 fi
 
 deploy_path=`pwd -P`
