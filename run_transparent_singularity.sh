73c32716dae5bb926f53c051fac2da2b4cf0de5f
Steffen <steffen.bollmann@cai.uq.edu.au>
2020-04-28 15:26:33 +1000

    might work for the bindpath:?

--- run_transparent_singularity.sh
+++ run_transparent_singularity.sh
@@ -157,7 +157,7 @@ if [ -d "$FILE" ]; then
    application_version=`echo $container | cut -d _ -f 2`
    echo "create start script for cvl"
    echo "#!/bin/bash" > cvl-${container}.sh
-   ## ADD MOUNTPOINTS HERE and test!
+   echo 'export SINGULARITY_BINDPATH="/state/,/RDS,/30days,/90days,/QRISdata,$SINGULARITY_BINDPATH"'
    echo "xterm -title '${application_name} ${application_version}' -e /bin/bash -c 'module load singularity/3.5.0;$deploy_path/$container'" >>  cvl-${container}.sh
    chmod 775 cvl-${container}.sh
    mv cvl-${container}.sh ../../bin
