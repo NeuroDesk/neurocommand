82f6758aa464ecca06ec2122209587dd01f64863
Aswin Narayanan <aswin@excelsol.com>
2020-06-23 20:38:44 +1000

    Container name and Installation dir as arguments

--- neurodesk.sh
+++ neurodesk.sh
@@ -1,15 +1,55 @@
-cat README.md 
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+   do
+   key="$1"
 
-echo Enter container filename:
-read containerName
+   case $key in
+      -d|--installdir)
+      installdir="$2"
+      shift # past argument
+      shift # past value
+      ;;
+      -c|--container)
+      containerName="$2"
+      shift # past argument
+      shift # past value
+      ;;
+      --default)
+      DEFAULT=YES
+      shift # past argument
+      ;;
+      *)    # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+   esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [[ -n $1 ]]; then
+    container="$1"
+fi
+
+if [ -z "$containerName" ]; then
+    cat README.md
+    echo Enter container filename:
+    read containerName
+fi
+
+if [ -z "$installdir" ]; then
+    echo Enter installation directory:
+    read installdir
+fi
+
+if [ -z "$installdir" ]; then
+    installdir=`pwd -P`
+fi
 
 # create folder structure for CVL
-if [ ! -d "containers" ]; then
-  mkdir -p containers
-  mkdir -p xdg_data_dirs/applications
-  mkdir -p xdg_data_dirs/desktop-directories
-  mkdir -p xdg_config_dirs/menus
-fi
+mkdir -p $installdir/containers
+mkdir -p $installdir/xdg_data_dirs/applications
+mkdir -p $installdir/xdg_data_dirs/desktop-directories
+mkdir -p $installdir/xdg_config_dirs/menus
 
 cd containers
 
