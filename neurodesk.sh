a8f603369621dbd354a29cb3c6bee504cb632494
Steffen <stebo85@web.de>
2020-07-10 11:29:14 +1000

    installdir variable

--- neurodesk.sh
+++ neurodesk.sh
@@ -40,22 +40,22 @@ echo "installing neurodesk in $installdir"
 if [ "$system_install" = "true" ]; then
     echo "replacing system files!"
     mkdir -p /root/.config/lxpanel/LXDE/icons
-    ln -s /neurodesk/menus/icons/* /root/.config/lxpanel/LXDE/icons/
+    ln -s ${installdir}/menus/icons/* /root/.config/lxpanel/LXDE/icons/
     chmod 644 /root/.config/lxpanel/LXDE/icons/*
 
     # Main-menu config. Add Menu changes to vnm-applications.menu
     rm -rf /etc/xdg/menus/lxde-applications.menu
-    mv /neurodesk/menus/lxde-applications.menu /etc/xdg/menus/
+    mv ${installdir}/menus/lxde-applications.menu /etc/xdg/menus/
     # THIS SHOULD BE INSERTED via the neurodesk install script	<!-- VNM Applications submenu -->
         #<MergeFile>vnm-applications.menu</MergeFile>
-    ln -s /neurodesk/menus/vnm-applications.menu /etc/xdg/menus/
+    ln -s ${installdir}/menus/vnm-applications.menu /etc/xdg/menus/
 
     chmod 644 /etc/xdg/menus/lxde-applications.menu
 
-    ln -s /neurodesk/menus/vnm-neuroimaging.directory /usr/share/desktop-directories/
-    ln -s /neurodesk/fetch_and_run.sh /usr/share/
+    ln -s ${installdir}/menus/vnm-neuroimaging.directory /usr/share/desktop-directories/
+    ln -s ${installdir}/fetch_and_run.sh /usr/share/
 
     # Build the menu
-    cd /neurodesk/menus
+    cd ${installdir}/menus
     python3 build_menu.py
 fi
