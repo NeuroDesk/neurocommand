b0b2686df557146ed45c9191c587344779b1af30
Steffen <stebo85@web.de>
2020-07-10 11:24:58 +1000

    moving commands from dockerfile to installer

--- neurodesk.sh
+++ neurodesk.sh
@@ -9,8 +9,8 @@ while [[ $# -gt 0 ]]
       shift # past argument
       shift # past value
       ;;
-      -c|--container)
-      containerName="$2"
+      -i|--system_install)
+      system_install="$2"
       shift # past argument
       shift # past value
       ;;
@@ -26,33 +26,36 @@ while [[ $# -gt 0 ]]
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
+# if there is only one argument use that as installdir
 if [[ -n $1 ]]; then
-    container="$1"
-fi
-
-if [ -z "$containerName" ]; then
-    cat README.md
-    echo Enter container filename:
-    read containerName
-fi
-
-if [ -z "$installdir" ]; then
-    echo Enter installation directory:
-    read installdir
+    installdir="$1"
 fi
 
 if [ -z "$installdir" ]; then
     installdir=`pwd -P`
 fi
 
-# create folder structure
-mkdir -p $installdir/containers
-mkdir -p $installdir/xdg_data_dirs/applications
-mkdir -p $installdir/xdg_data_dirs/desktop-directories
-mkdir -p $installdir/xdg_config_dirs/menus
+echo "installing neurodesk in $installdir"
 
-cd containers
+if [ "$system_install" = "true" ]; then
+    echo "replacing system files!"
+    mkdir -p /root/.config/lxpanel/LXDE/icons
+    ln -s /neurodesk/menus/icons/* /root/.config/lxpanel/LXDE/icons/
+    chmod 644 /root/.config/lxpanel/LXDE/icons/*
 
-git clone https://github.com/neurodesk/transparent-singularity.git "${containerName%.sif}"
-cd "${containerName%.sif}"
-./run_transparent_singularity.sh $containerName --cvl true
+    # Main-menu config. Add Menu changes to vnm-applications.menu
+    rm -rf /etc/xdg/menus/lxde-applications.menu
+    mv /neurodesk/menus/lxde-applications.menu /etc/xdg/menus/
+    # THIS SHOULD BE INSERTED via the neurodesk install script	<!-- VNM Applications submenu -->
+        #<MergeFile>vnm-applications.menu</MergeFile>
+    ln -s /neurodesk/menus/vnm-applications.menu /etc/xdg/menus/
+
+    chmod 644 /etc/xdg/menus/lxde-applications.menu
+
+    ln -s /neurodesk/menus/vnm-neuroimaging.directory /usr/share/desktop-directories/
+    ln -s /neurodesk/fetch_and_run.sh /usr/share/
+
+    # Build the menu
+    cd /neurodesk/menus
+    python3 build_menu.py
+fi
