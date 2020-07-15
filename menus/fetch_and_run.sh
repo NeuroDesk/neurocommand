662cba90a324700d2f40be47e01cbc77431946c8
Steffen <stebo85@web.de>
2020-07-15 10:54:06 +1000

    typo

--- menus/fetch_and_run.sh
+++ menus/fetch_and_run.sh
@@ -60,7 +60,7 @@ if [ $# -le 3 ]; then
     CONTAINER_FILE_NAME=${CONTAINER_PATH}/${IMG_NAME}/${IMG_NAME}.sif
     if [ -f "${CONTAINER_FILE_NAME}" ]; then
         echo "attempting to launch container ${IMG_NAME}"
-        .${CONTAINER_FILE_NAME}
+        ${CONTAINER_FILE_NAME}
     else 
         echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
         echo "the container you have has a bug and needs to be updated on your system. To trigger a reinstall, run:"
