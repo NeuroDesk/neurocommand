83e76289aa270af10ccf4a9ad340b0a2cc5a7bad
Steffen <stebo85@web.de>
2020-08-10 17:15:49 +1000

    IMAGENAME_BUILDDATE

--- .github/workflows/check-singularity.sh
+++ .github/workflows/check-singularity.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IMAGENAME_BUILDDATE=$1
-if curl --output /dev/null --silent --head --fail "https://swift.rc.nectar.org.au:8888/v1/AUTH_d6165cc7b52841659ce8644df1884d5e/singularityImages/${IMAGENAMENAME}_${BUILDDATE}.sif"; then
+if curl --output /dev/null --silent --head --fail "https://swift.rc.nectar.org.au:8888/v1/AUTH_d6165cc7b52841659ce8644df1884d5e/singularityImages/${IMAGENAME_BUILDDATE}.sif"; then
     echo "${IMAGENAME_BUILDDATE}.sif exists"
     exit 0
 else
