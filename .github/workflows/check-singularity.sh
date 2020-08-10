eb271749a09a5ca9cb71465864f37610f00b47de
Steffen <stebo85@web.de>
2020-08-10 17:11:22 +1000

    grab variable

--- .github/workflows/check-singularity.sh
+++ .github/workflows/check-singularity.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+IMAGENAME_BUILDDATE=$1
 if curl --output /dev/null --silent --head --fail "https://swift.rc.nectar.org.au:8888/v1/AUTH_d6165cc7b52841659ce8644df1884d5e/singularityImages/${IMAGENAMENAME}_${BUILDDATE}.sif"; then
-    echo "${IMAGENAME}_${BUILDDATE}.sif exists"
+    echo "${IMAGENAME_BUILDDATE}.sif exists"
     exit 0
 else
-    echo "${IMAGENAME}_${BUILDDATE}.sif does not exist yet"
+    echo "${IMAGENAME_BUILDDATE}.sif does not exist yet"
     exit 2
 fi
