7f3d8e67923fa888e1e5a172b7a4563125215f5b
Steffen <stebo85@web.de>
2020-08-27 11:44:35 +1000

    only delete files if directories actually exist

--- neurodesk.sh
+++ neurodesk.sh
@@ -45,8 +45,8 @@ cd ${installdir}/menus
 
 # start fresh from template:
 cp vnm-applications.menu.template vnm-applications.menu
-rm applications/vnm-*
-rm desktop-directories/vnm-*
+[ -d applications ] && rm applications/vnm-*
+[ -d desktop-directories ] && rm desktop-directories/vnm-*
 
 python3 build_menu.py
 
