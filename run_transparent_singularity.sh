ee364ce4409ee157951e9e820d16b8b31c8847aa
Steffen Bollmann <stebo85@users.noreply.github.com>
2020-09-29 10:24:21 +1000

    container ending sif not working yet

--- run_transparent_singularity.sh
+++ run_transparent_singularity.sh
@@ -78,13 +78,16 @@ containerDate="$(cut -d'.' -f1 <<< ${containerDateAndFileEnding})"
 containerEnding="$(cut -d'.' -f2 <<< ${containerDateAndFileEnding})"
 
 echo "containerDate: ${containerDate}"
-echo "containerEnding: ${containerEnding}"
 
 # if no container extension is given, assume .sif
-if [ -z "$containerEnding" ]; then
-   containerEnding=".sif"
+if [ "$containerEnding" ="$containerDate" ]; then
+   containerEnding="sif"
    container=${containerName}_${containerVersion}-${containerDate}.${containerEnding}
 fi
+
+echo "containerEnding: ${containerEnding}"
+echo "trying if $container exists in the cache"
+
 
 # check if image is available on singularity cache:
 if curl --output /dev/null --silent --head --fail "https://swift.rc.nectar.org.au:8888/v1/AUTH_d6165cc7b52841659ce8644df1884d5e/singularityImages/$container"; then
