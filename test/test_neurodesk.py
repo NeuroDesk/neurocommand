fa47dc84223f383f5a5dd88684aed4d57b03bdca
Aswin Narayanan <aswin@excelsol.com>
2020-10-06 22:25:47 +1000

    Intial unittest

---
+++ test/test_neurodesk.py
@@ -0,0 +1,20 @@
+
+
+from neurodesk import vnm_xml
+
+import xml.etree.ElementTree as et
+import filecmp
+
+from pathlib import Path
+import pytest
+
+def test_vnm_xml(tmp_path):
+    # Read in test applications.menu (for lxde)
+    xml = Path('test/lxde-applications.menu').resolve(strict=True)
+    et.parse(xml)
+
+    newxml = tmp_path/'new.xml'
+    vnm_xml(xml, newxml)
+    et.parse(newxml)
+    # Assert change in xml file
+    assert not filecmp.cmp(xml, newxml)
