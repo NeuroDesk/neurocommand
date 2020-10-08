791e518cd515d210e2d6e3adc69595d5b192ee82
Aswin Narayanan <aswin@excelsol.com>
2020-10-08 18:21:31 +1000

    Add edit option to config to disable automatic menu editing

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ source neurodesk/configparser.sh
 
 echo "WARNING: Will modify/replace system files!!!"
 read -p "Press enter to continue ..."
-if [ -n "${vnm[appmenu]}" ]; then
+if [ "${vnm[edit]}" == "y" ]; then
     mv -vn ${vnm[appmenu]} ${vnm[appmenu]}.BAK
+    ln -s ${vnm[installdir]}/$appmenufile $appmenudir
+else 
+    echo "!!! Add <MergeFile>vnm-applications.menu</MergeFile> to ${vnm[appmenu]} !!!"
 fi
-ln -s ${vnm[installdir]}/$appmenufile $appmenudir
 ln -s ${vnm[installdir]}/vnm-applications.menu $appmenudir
 
 for file in ${vnm[installdir]}/applications/vnm-*.desktop; do
--- uninstall.sh
+++ uninstall.sh
@@ -4,12 +4,14 @@ source neurodesk/configparser.sh
 
 echo "WARNING: Will modify/replace system files!!!"
 read -p "Press enter to continue ..."
-rm_symlink ${vnm[appmenu]}
-rm_symlink $appmenudir/vnm-applications.menu
 
-if [ -n "${vnm[appmenu]}" ]; then
-mv -vn ${vnm[appmenu]}.BAK ${vnm[appmenu]}
+if [ "${vnm[edit]}" == "y" ]; then
+    rm_symlink ${vnm[appmenu]}
+    mv -vn ${vnm[appmenu]}.BAK ${vnm[appmenu]}
+else 
+    echo "!!! Remove <MergeFile>vnm-applications.menu</MergeFile> from ${vnm[appmenu]} !!!"
 fi
+rm_symlink $appmenudir/vnm-applications.menu
 for file in ${vnm[appdir]}/vnm-*.desktop; do
     rm_symlink $file
 done
