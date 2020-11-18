0c036a63a2fd41f88c2a9a597a09ad06a7bfed17
Aswin Narayanan <aswin@excelsol.com>
2020-11-18 12:16:00 +1000

    Fix menu recursion bug Bug caused by applications symlink being nested into local/applications. Use -sfn to correct repeat symlink behavior

--- install.sh
+++ install.sh
@@ -6,15 +6,14 @@ echo "WARNING: Will modify/replace system files!!!"
 # read -p "Press enter to continue ..."
 
 if [ "${vnm_edit}" == "y" ]; then
-    mv -vn ${vnm_appmenu} ${vnm_appmenu}.BAK
-    ln -s ${vnm_installdir}/${vnm_appmenufile} ${vnm_appmenudir}
+    mv -vn ${vnm_appmenu} ${vnm_appmenu}.BAK    ln -sfn ${vnm_installdir}/${vnm_appmenufile} ${vnm_appmenudir}
 else 
     echo "!!! Add <MergeFile>vnm-applications.menu</MergeFile> to ${vnm_appmenu} !!!"
 fi
-ln -s ${vnm_installdir}/vnm-applications.menu ${vnm_appmenudir}
+ln -sfn ${vnm_installdir}/vnm-applications.menu ${vnm_appmenudir}
 
-ln -s ${vnm_installdir}/applications ${vnm_appdir}/vnm
-ln -s ${vnm_installdir}/desktop-directories ${vnm_deskdir}/vnm
+ln -sfn ${vnm_installdir}/applications ${vnm_appdir}/vnm
+ln -sfn ${vnm_installdir}/desktop-directories ${vnm_deskdir}/vnm
 
 #for file in ${vnm_installdir}/applications/vnm-*.desktop; do
 #    ln -s $file ${vnm_appdir}
