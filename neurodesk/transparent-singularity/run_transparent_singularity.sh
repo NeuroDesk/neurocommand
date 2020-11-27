2f053f5f63f9935702f15be57a2076b2fd83d167
Steffen Bollmann <stebo85@users.noreply.github.com>
2020-11-27 20:45:06 +1000

    don't try to module load singularity - user has to make sure it's loaded correctly

--- neurodesk/transparent-singularity/run_transparent_singularity.sh
+++ neurodesk/transparent-singularity/run_transparent_singularity.sh
@@ -93,12 +93,6 @@ qq=`which  singularity`
 if [[  ${#qq} -lt 1 ]]; then
    echo "This script requires singularity on your path. E.g. add module load singularity/2.4.2 to your .bashrc"
    echo "If you are root try again as normal user"
-   echo "trying to module load:"
-   module load singularity
-   qq=`which  singularity`
-   if [[  ${#qq} -lt 1 ]]; then
-      echo "ERROR: SOMETHING IS WRONG WITH SINGULARITY"
-   fi
 fi
 
 # check if singularity is installed in the correct version
