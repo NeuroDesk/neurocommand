ea9ce8491f96f6a43e9e823f72b339164dac6bf3
Steffen Bollmann <stebo85@users.noreply.github.com>
2020-12-21 13:18:26 +1000

    change to older container format for compatibility

--- .github/workflows/upload_containers_oracle.sh
+++ .github/workflows/upload_containers_oracle.sh
@@ -28,42 +28,7 @@ sudo apt-get install singularity-container
 while IFS= read -r IMAGENAME_BUILDDATE
 do
   echo "$IMAGENAME_BUILDDATE"
-    #build singularity 3 image
-    if curl --output /dev/null --silent --head --fail "https://objectstorage.us-ashburn-1.oraclecloud.com/n/nrrir2sdpmdp/b/neurodesk/o/${IMAGENAME_BUILDDATE}.sif"; then
-        echo "${IMAGENAME_BUILDDATE}.sif exists"
-    else
-        echo "${IMAGENAME_BUILDDATE}.sif does not exist yet - building it!"
-        
-        echo "[DEBUG] DOCKERHUB_ORG: $DOCKERHUB_ORG"
-        REGISTRY=$(echo docker.pkg.github.com/$GITHUB_REPOSITORY | tr '[A-Z]' '[a-z]')
-        echo "[DEBUG] REGISTRY: $REGISTRY"
-        IMAGEID="$DOCKERHUB_ORG/$IMAGENAME_BUILDDATE"
-        echo "[DEBUG] IMAGEID: $IMAGEID"
-        IMAGENAME="$(cut -d'_' -f1,2 <<< ${IMAGENAME_BUILDDATE})"
-        BUILDDATE="$(cut -d'_' -f3 <<< ${IMAGENAME_BUILDDATE})"
-        echo "[DEBUG] IMAGENAME: $IMAGENAME"
-        echo "[DEBUG] BUILDDATE: $BUILDDATE"
-
-        echo "[DEBUG] Pulling latest build of singularity ..."
-        docker pull $REGISTRY/singularity3
-        echo "[DEBUG] Build singularity container ..."
-        echo "[DEBUG] docker run -v $HOME:/home $REGISTRY/singularity build /home/${IMAGENAME_BUILDDATE}.sif docker://$DOCKERHUB_ORG/$IMAGENAME:$BUILDDATE"
-        docker run -v $HOME:/home $REGISTRY/singularity3 build "/home/${IMAGENAME_BUILDDATE}.sif" docker://$DOCKERHUB_ORG/$IMAGENAME:$BUILDDATE
-
-        echo "[DEBUG] Attempting upload to Oracle ..."
-        curl -v -X PUT -u ${ORACLE_USER} --upload-file $HOME/${IMAGENAME_BUILDDATE}.sif $ORACLE_NEURODESK_BUCKET
-        rm $HOME/${IMAGENAME_BUILDDATE}.sif
-        df -h
-
-        if curl --output /dev/null --silent --head --fail "https://objectstorage.us-ashburn-1.oraclecloud.com/n/nrrir2sdpmdp/b/neurodesk/o/${IMAGENAME_BUILDDATE}.sif"; then
-            echo "${IMAGENAME_BUILDDATE}.sif was freshly build and exists now :)"
-        else
-            echo "${IMAGENAME_BUILDDATE}.sif does not exist yet. Something is WRONG"
-            exit 2
-        fi
-    fi
-
-    #build singularity 2 image
+    #build singularity 2 image (as this will also be compatible to 3.x.y)
     if curl --output /dev/null --silent --head --fail "https://objectstorage.us-ashburn-1.oraclecloud.com/n/nrrir2sdpmdp/b/neurodesk/o/${IMAGENAME_BUILDDATE}.simg"; then
         echo "${IMAGENAME_BUILDDATE}.simg exists"
     else
@@ -73,7 +38,7 @@ do
         BUILDDATE="$(cut -d'_' -f3 <<< ${IMAGENAME_BUILDDATE})"
         echo "[DEBUG] IMAGENAME: $IMAGENAME"
         echo "[DEBUG] BUILDDATE: $BUILDDATE"
-        sudo singularity build "/home/${IMAGENAME_BUILDDATE}.simg" docker://$DOCKERHUB_ORG/$IMAGENAME:$BUILDDATE
+        sudo singularity build "$HOME/${IMAGENAME_BUILDDATE}.simg" docker://docker.pkg.github.com/neurodesk/caid/$IMAGENAME:$BUILDDATE
 
         echo "[DEBUG] Attempting upload to Oracle ..."
         curl -v -X PUT -u ${ORACLE_USER} --upload-file $HOME/${IMAGENAME_BUILDDATE}.simg $ORACLE_NEURODESK_BUCKET
