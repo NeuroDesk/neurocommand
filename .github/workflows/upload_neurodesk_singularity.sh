2186d5d3a8c97f59197663eaa48227de639c1167
Steffen Bollmann <stebo85@users.noreply.github.com>
2020-12-21 15:12:41 +1000

    added wheel + cleanup

--- .github/workflows/upload_neurodesk_singularity.sh
+++ .github/workflows/upload_neurodesk_singularity.sh
@@ -1,53 +1,9 @@
 #!/usr/bin/env bash
 # set -e
-
-echo "checking if containers are built"
-
-#creating logfile with available containers
-cd neurodesk
-python3 write_log.py
-
-# remove empty lines
-sed -i '/^$/d' log.txt
-
-# remove square brackets
-sed -i 's/[][]//g' log.txt
-
-# remove spaces around
-sed -i -e 's/^[ \t]*//' -e 's/[ \t]*$//' log.txt
-
-# replace spaces with underscores
-sed -i 's/ /_/g' log.txt
-
-#setup singularity 2.6.1 from neurodebian
-wget -O- http://neuro.debian.net/lists/bionic.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
-sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
-sudo apt-get update
-sudo apt-get install singularity-container
-
-echo "[DEBUG] Configure for SWIFT storage"
-pip install wheel
-pip install python-swiftclient python-keystoneclient
-export OS_AUTH_URL=https://keystone.rc.nectar.org.au:5000/v3/
-export OS_AUTH_TYPE=v3applicationcredential
-export OS_PROJECT_NAME="CAI_Container_Builder"
-export OS_USER_DOMAIN_NAME="Default"
-export OS_REGION_NAME="Melbourne"
-
-export IMAGE_HOME="/home/runner"
-
-while IFS= read -r IMAGENAME_BUILDDATE
-do
-    echo "$IMAGENAME_BUILDDATE"
-    echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg does not exist yet - building it!"
-    echo "[DEBUG] DOCKERHUB_ORG: $DOCKERHUB_ORG"
-    IMAGENAME="$(cut -d'_' -f1,2 <<< ${IMAGENAME_BUILDDATE})"
-    BUILDDATE="$(cut -d'_' -f3 <<< ${IMAGENAME_BUILDDATE})"
-    echo "[DEBUG] IMAGENAME: $IMAGENAME"
-    echo "[DEBUG] BUILDDATE: $BUILDDATE"
-
+    export IMAGENAME="neurodesk"
+    export BUILDDATE="latest"
     # Oracle Ashburn (with cloud mirror to Zurich)
-    if curl --output /dev/null --silent --head --fail "https://objectstorage.us-ashburn-1.oraclecloud.com/n/nrrir2sdpmdp/b/neurodesk/o/${IMAGENAME_BUILDDATE}.simg"; then
+    if curl --output /dev/null --silent --head --fail "https://objectstorage.us-ashburn-1.oraclecloud.com/n/nrrir2sdpmdp/b/neurodesk/o/${IMAGENAME}_${BUILDDATE}.simg"; then
         echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg exists in ashburn oracle cloud"
     else
 
@@ -86,4 +42,3 @@ do
             exit 2
         fi
     fi
-done < log.txt
