04d5d69938c4cbb382fc797c8bb73ea546157f7b
Steffen Bollmann <stebo85@users.noreply.github.com>
2020-12-21 14:25:08 +1000

    test

--- .github/workflows/upload_containers_oracle.sh
+++ .github/workflows/upload_containers_oracle.sh
@@ -25,31 +25,62 @@ sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D3
 sudo apt-get update
 sudo apt-get install singularity-container
 
+echo "[DEBUG] Configure for SWIFT storage"
+pip install python-swiftclient python-keystoneclient
+export OS_AUTH_URL=https://keystone.rc.nectar.org.au:5000/v3/
+export OS_AUTH_TYPE=v3applicationcredential
+export OS_PROJECT_NAME="CAI_Container_Builder"
+export OS_USER_DOMAIN_NAME="Default"
+export OS_REGION_NAME="Melbourne"
+
+export IMAGE_HOME="/home/runner/"
+
 while IFS= read -r IMAGENAME_BUILDDATE
 do
-  echo "$IMAGENAME_BUILDDATE"
-    #build singularity 2 image (as this will also be compatible to 3.x.y)
+    echo "$IMAGENAME_BUILDDATE"
+
+    # Oracle Ashburn (with cloud mirror to Zurich)
     if curl --output /dev/null --silent --head --fail "https://objectstorage.us-ashburn-1.oraclecloud.com/n/nrrir2sdpmdp/b/neurodesk/o/${IMAGENAME_BUILDDATE}.simg"; then
-        echo "${IMAGENAME_BUILDDATE}.simg exists"
+        echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg exists in ashburn oracle cloud"
     else
-        echo "${IMAGENAME_BUILDDATE}.simg does not exist yet - building it!"
+        echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg does not exist yet - building it!"
         echo "[DEBUG] DOCKERHUB_ORG: $DOCKERHUB_ORG"
         IMAGENAME="$(cut -d'_' -f1,2 <<< ${IMAGENAME_BUILDDATE})"
         BUILDDATE="$(cut -d'_' -f3 <<< ${IMAGENAME_BUILDDATE})"
         echo "[DEBUG] IMAGENAME: $IMAGENAME"
         echo "[DEBUG] BUILDDATE: $BUILDDATE"
-        sudo singularity build "$HOME/${IMAGENAME_BUILDDATE}.simg" docker://$DOCKERHUB_ORG/$IMAGENAME:$BUILDDATE
+        sudo singularity build "$IMAGE_HOME/${IMAGENAME_BUILDDATE}.simg" docker://$DOCKERHUB_ORG/$IMAGENAME:$BUILDDATE
 
         echo "[DEBUG] Attempting upload to Oracle ..."
-        curl -v -X PUT -u ${ORACLE_USER} --upload-file $HOME/${IMAGENAME_BUILDDATE}.simg $ORACLE_NEURODESK_BUCKET
-        rm $HOME/${IMAGENAME_BUILDDATE}.simg
-        rm -rf /home/runner/.singularity/docker
-        df -h
+        curl -v -X PUT -u ${ORACLE_USER} --upload-file $IMAGE_HOME/${IMAGENAME_BUILDDATE}.simg $ORACLE_NEURODESK_BUCKET
 
         if curl --output /dev/null --silent --head --fail "https://objectstorage.us-ashburn-1.oraclecloud.com/n/nrrir2sdpmdp/b/neurodesk/o/${IMAGENAME_BUILDDATE}.simg"; then
             echo "${IMAGENAME_BUILDDATE}.simg was freshly build and exists now :)"
         else
             echo "${IMAGENAME_BUILDDATE}.simg does not exist yet. Something is WRONG"
+            exit 2
+        fi
+    fi
+
+    # Nectar Swift
+    if curl --output /dev/null --silent --head --fail "https://swift.rc.nectar.org.au:8888/v1/AUTH_d6165cc7b52841659ce8644df1884d5e/singularityImages/${IMAGENAME_BUILDDATE}.simg"; then
+        echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg exists in swift storage"
+    else
+        echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg does not exist yet in nectar swift - building it!"
+        if [[ ! -f $IMAGE_HOME/${IMAGENAME_BUILDDATE}.simg ]]; then
+            sudo singularity build "$IMAGE_HOME/${IMAGENAME_BUILDDATE}.simg" docker://$DOCKERHUB_ORG/$IMAGENAME:$BUILDDATE
+        fi
+
+        echo "[DEBUG] Attempting upload to nectar swift ..."
+        swift upload singularityImages $IMAGE_HOME/${IMAGENAME_BUILDDATE}.simg --segment-size 1073741824
+
+        if curl --output /dev/null --silent --head --fail "https://swift.rc.nectar.org.au:8888/v1/AUTH_d6165cc7b52841659ce8644df1884d5e/singularityImages/${IMAGENAME_BUILDDATE}.simg"; then
+            echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg was freshly build and exists now. Cleaning UP! :)"
+            rm $IMAGE_HOME/${IMAGENAME_BUILDDATE}.simg
+            sudo rm -rf /root/.singularity/docker
+            df -h
+        else
+            echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg does not exist yet. Something is WRONG"
             exit 2
         fi
     fi
