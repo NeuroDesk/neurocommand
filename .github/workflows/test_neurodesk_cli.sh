95648e765a8569734bdc18f44696bd360652734c
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-01-04 14:35:58 +1000

    comment set -e to allow failure on key

--- .github/workflows/test_neurodesk_cli.sh
+++ .github/workflows/test_neurodesk_cli.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+# set -e
 
 #setup singularity 2.6.1 from neurodebian
 wget -O- http://neuro.debian.net/lists/bionic.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
