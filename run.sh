664d06b4755b92f852f1ca49c7b051756e8452ff
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-01-05 14:45:52 +1000

    upload neurodesk container again

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
-#test for singularity install otherwise try to module load
+#test for singularity install
+echo "checking for singularity ..."
+qq=`which  singularity`
+if [[  ${#qq} -lt 1 ]]; then
+   echo "This script requires singularity on your path. E.g. add module load singularity/2.4.2 to your .bashrc"
+   echo "If you are root try again as normal user"
+   else
+   echo "... found :)"
+fi
 
 #test for wget, curl, or aria2
 
 #download neurodesk container
+
+#run neurodesk container
