5b39b14de2c9cd18a64dda1fec92836825b019f6
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-01-06 09:48:31 +1000

    singularity container is named singularity3 due to name conflict

--- .github/workflows/upload_neurodesk_singularity.sh
+++ .github/workflows/upload_neurodesk_singularity.sh
@@ -112,8 +112,8 @@ else
 
         echo "[DEBUG] Pulling latest build of singularity ..."
         docker pull $REGISTRY/singularity3
-        echo "[DEBUG] Build singularity container ..."
-        echo "[DEBUG] docker run -v $HOME:/home $REGISTRY/singularity build /home/${IMAGENAME}_${BUILDDATE}.sif docker://$DOCKERHUB_ORG/$IMAGENAME"
+        echo "[DEBUG] Build singularity sif container ..."
+        echo "[DEBUG] docker run -v $HOME:/home $REGISTRY/singularity3 build /home/${IMAGENAME}_${BUILDDATE}.sif docker://$DOCKERHUB_ORG/$IMAGENAME"
         docker run -v $HOME:/home $REGISTRY/singularity build "/home/${IMAGENAME}_${BUILDDATE}.sif" docker://$DOCKERHUB_ORG/$IMAGENAME
         sudo singularity build "$IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.sif" docker://$DOCKERHUB_ORG/$IMAGENAME:$BUILDDATE
     fi
