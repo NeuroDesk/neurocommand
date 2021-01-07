07df5667e26ab4f1a81255230b81fb281983a1d1
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-01-07 10:13:13 +1000

    more action cleanup

--- .github/workflows/pull_latest_container_from_gh_packages.sh
+++ .github/workflows/pull_latest_container_from_gh_packages.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 # set -e
-        IMAGEID=docker.pkg.github.com/$GITHUB_REPOSITORY/$1
-        IMAGEID=$(echo $IMAGEID | tr '[A-Z]' '[a-z]')
-        {
-          docker pull $IMAGEID \
-            && ROOTFS_CACHE=$(docker inspect --format='{{.RootFS}}' $IMAGEID) \
-            && echo "ROOTFS_CACHE=$ROOTFS_CACHE" >> $GITHUB_ENV
-        } || echo "[DEBUG] $IMAGEID not found. Resuming build..."
-        echo "IMAGEID=$IMAGEID" >> $GITHUB_ENV
+echo "[DEBUG] docker GITHUB_REPOSITORY: $GITHUB_REPOSITORY"
+echo "[DEBUG] docker package to pull: $1"
+IMAGEID=docker.pkg.github.com/$GITHUB_REPOSITORY/$1
+IMAGEID=$(echo $IMAGEID | tr '[A-Z]' '[a-z]')
+{
+  docker pull $IMAGEID \
+    && ROOTFS_CACHE=$(docker inspect --format='{{.RootFS}}' $IMAGEID) \
+    && echo "ROOTFS_CACHE=$ROOTFS_CACHE" >> $GITHUB_ENV
+} || echo "[DEBUG] $IMAGEID not found. Resuming build..."
+echo "IMAGEID=$IMAGEID" >> $GITHUB_ENV
