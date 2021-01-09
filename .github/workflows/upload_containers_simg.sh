2a6e562f840753fb0c2c4adc09609ca7a59ad5c8
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-01-09 18:41:21 +1000

    don't setup singularity if not needed

--- .github/workflows/upload_containers_simg.sh
+++ .github/workflows/upload_containers_simg.sh
@@ -18,23 +18,6 @@ sed -i -e 's/^[ \t]*//' -e 's/[ \t]*$//' log.txt
 # replace spaces with underscores
 sed -i 's/ /_/g' log.txt
 
-#setup singularity 2.6.1 from neurodebian
-wget -O- http://neuro.debian.net/lists/bionic.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
-sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
-sudo apt-get update
-sudo apt-get install singularity-container
-
-echo "[DEBUG] Configure for SWIFT storage"
-sudo pip3 install wheel
-sudo pip3 install python-swiftclient python-keystoneclient
-export OS_AUTH_URL=https://keystone.rc.nectar.org.au:5000/v3/
-export OS_AUTH_TYPE=v3applicationcredential
-export OS_PROJECT_NAME="CAI_Container_Builder"
-export OS_USER_DOMAIN_NAME="Default"
-export OS_REGION_NAME="Melbourne"
-
-export IMAGE_HOME="/home/runner"
-
 echo "$GITHUB_TOKEN" | docker login docker.pkg.github.com -u $GITHUB_ACTOR --password-stdin
 echo "$DOCKERHUB_PASSWORD" | docker login -u $DOCKERHUB_USERNAME --password-stdin
 
@@ -49,13 +32,35 @@ do
     if curl --output /dev/null --silent --head --fail "https://objectstorage.us-ashburn-1.oraclecloud.com/n/nrrir2sdpmdp/b/neurodesk/o/${IMAGENAME_BUILDDATE}.simg"; then
         echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg exists in ashburn oracle cloud"
     else
-        check if there is enough free disk space on the runner:
+        # check if there is enough free disk space on the runner:
         FREE=`df -k --output=avail "$PWD" | tail -n1`   # df -k not df -h
         echo "[DEBUG] This runner has ${FREE} free disk space"
         if [[ $FREE -lt 30485760 ]]; then               # 30G = 10*1024*1024k
             echo "[DEBUG] This runner has not enough free disk space .. cleaning up!"
             bash .github/workflows/free-up-space.sh
         fi;
+
+        if [ -n "$singularity_setup_done" ]; then
+            echo "Setup already done. Skipping."
+        else
+            #setup singularity 2.6.1 from neurodebian
+            wget -O- http://neuro.debian.net/lists/bionic.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+            sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+            sudo apt-get update
+            sudo apt-get install singularity-container
+
+            echo "[DEBUG] Configure for SWIFT storage"
+            sudo pip3 install wheel
+            sudo pip3 install python-swiftclient python-keystoneclient
+            export OS_AUTH_URL=https://keystone.rc.nectar.org.au:5000/v3/
+            export OS_AUTH_TYPE=v3applicationcredential
+            export OS_PROJECT_NAME="CAI_Container_Builder"
+            export OS_USER_DOMAIN_NAME="Default"
+            export OS_REGION_NAME="Melbourne"
+
+            export IMAGE_HOME="/home/runner"
+            export singularity_setup_done="true"
+        fi
 
         echo "[DEBUG] singularity building docker://vnmd/$IMAGENAME:$BUILDDATE"
         sudo singularity build "$IMAGE_HOME/${IMAGENAME_BUILDDATE}.simg"  docker://vnmd/$IMAGENAME:$BUILDDATE
@@ -83,6 +88,24 @@ do
             echo "[DEBUG] This runner has not enough free disk space .. cleaning up!"
             bash .github/workflows/free-up-space.sh
         fi;
+
+        if [ -n "$swift_setup_done" ]; then
+            echo "Setup already done. Skipping."
+        else
+            echo "[DEBUG] Configure for SWIFT storage"
+            sudo pip3 install wheel
+            sudo pip3 install python-swiftclient python-keystoneclient
+            export OS_AUTH_URL=https://keystone.rc.nectar.org.au:5000/v3/
+            export OS_AUTH_TYPE=v3applicationcredential
+            export OS_PROJECT_NAME="CAI_Container_Builder"
+            export OS_USER_DOMAIN_NAME="Default"
+            export OS_REGION_NAME="Melbourne"
+
+            export IMAGE_HOME="/home/runner"
+            export swift_setup_done="true"
+        fi
+
+
         echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg does not exist locally - pulling it from oracle cloud!"
         if [[ ! -f $IMAGE_HOME/${IMAGENAME_BUILDDATE}.simg ]]; then
             curl -X GET https://objectstorage.eu-zurich-1.oraclecloud.com/n/nrrir2sdpmdp/b/neurodesk/o/${IMAGENAME_BUILDDATE}.simg -O $IMAGE_HOME/${IMAGENAME_BUILDDATE}.simg
