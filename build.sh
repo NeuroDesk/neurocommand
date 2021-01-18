6560f70eeab99ff6c465d8a14e3c69a961015e28
Aswin Narayanan <aswin@excelsol.com>
2021-01-18 22:08:41 +1000

    File/Dir verification in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-python -m neurodesk $@
+# python -m neurodesk $@
+
+# https://stackoverflow.com/a/21188136
+get_abs_filename() {
+  # $1 : relative filename
+  filename=$1
+  parentdir=$(dirname "${filename}")
+
+  if [ -d "${filename}" ]; then
+      echo "$(cd "${filename}" && pwd)"
+  elif [ -d "${parentdir}" ]; then
+    echo "$(cd "${parentdir}" && pwd)/$(basename "${filename}")"
+  fi
+}
+# read -p "deskenv: " deskenv
+read -e -p "installdir: " installdir
+# read -p "appmenu: " appmenu
+# read -p "appdir: " appdir
+# read -p "deskdir: " deskdir
+# read -p "edit: " edit
+# read -p "sh_prefix: " sh_prefix
+
+get_abs_filename $installdir
+readlink -f $installdir
+realpath $installdir
+
+case $installdir in
+  /*) echo "absolute path" ;;
+  *) echo "something else" ;;
+esac
