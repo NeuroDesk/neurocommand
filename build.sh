c9c4d1ea2b0a7ccf71fff837b62a6ba1f6a1bfce
Aswin Narayanan <aswin@excelsol.com>
2021-01-19 15:12:31 +1000

    Eval abs path in build.sh

--- build.sh
+++ build.sh
@@ -2,31 +2,37 @@
 
 # python -m neurodesk $@
 
-# https://stackoverflow.com/a/21188136
-get_abs_filename() {
-  # $1 : relative filename
-  filename=$1
-  parentdir=$(dirname "${filename}")
-
-  if [ -d "${filename}" ]; then
-      echo "$(cd "${filename}" && pwd)"
-  elif [ -d "${parentdir}" ]; then
-    echo "$(cd "${parentdir}" && pwd)/$(basename "${filename}")"
-  fi
+get_abs_filepath() {
+    path="${1/#\~/$HOME}"
+    parentdir=$(dirname "${path}")
+    name=$(basename "${path}")
+    echo "$(cd ${parentdir} && pwd -P)/${name}"
 }
-# read -p "deskenv: " deskenv
+
+# Installation Directory [./local]
 read -e -p "installdir: " installdir
-# read -p "appmenu: " appmenu
-# read -p "appdir: " appdir
-# read -p "deskdir: " deskdir
-# read -p "edit: " edit
-# read -p "sh_prefix: " sh_prefix
+get_abs_filepath $installdir
 
-get_abs_filename $installdir
-readlink -f $installdir
-realpath $installdir
+if [[ ! -d "$installdir" ]]; then
+  echo "Installation Dir $installdir does not exist. Creating ..."
+  mkdir $installdir
+fi
 
-case $installdir in
-  /*) echo "absolute path" ;;
-  *) echo "something else" ;;
-esac
+exit
+# Desktop Environment [cli/lxde/mate]
+read -p "deskenv: " deskenv
+
+# Applications Menu
+read -e -p "appmenu: " appmenu
+appmenu=$(get_abs_filepath $appmenu)
+
+# Applications Directory
+read -e -p "appdir: " appdir
+appdir=$(get_abs_dirpath $appdir)
+
+# Desktop Directories
+read -e -p "deskdir: " deskdir
+deskdir=$(get_abs_dirpath $deskdir)
+
+# Edit mode [y/n]
+read -p "edit : " edit
