e4260e199bcb96bf2da5c55d131229a567515843
Aswin Narayanan <aswin@excelsol.com>
2021-01-19 15:54:43 +1000

    Input resolver for installdir

--- build.sh
+++ build.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
 # python -m neurodesk $@
-
-get_abs_filepath() {
+resolve_abs_parentdir() {
     path="${1/#\~/$HOME}"
     parentdir=$(dirname "${path}")
     name=$(basename "${path}")
     echo "$(cd ${parentdir} && pwd -P)/${name}"
 }
 
+resolve_abs_path() {
+    path="${1/#\~/$HOME}"
+    name=$(basename "${path}")
+    echo "$(cd ${path} && pwd -P)"
+}
+
 # Installation Directory [./local]
 read -e -p "installdir: " installdir
-get_abs_filepath $installdir
-
-if [[ ! -d "$installdir" ]]; then
-  echo "Installation Dir $installdir does not exist. Creating ..."
-  mkdir $installdir
+if [ -z "$installdir" ]; then
+    installdir="$(pwd -P)"
 fi
+installdir=$(resolve_abs_parentdir ${installdir})
+if [ ! -d "$installdir" ]; then
+    echo "Installation directory does not exist"
+    echo "Creating $installdir"
+    mkdir -p $installdir
+fi
+installdir=$(resolve_abs_path ${installdir})
+if [ "$installdir" == $(pwd -P) ]; then
+    installdir="$(pwd -P)/local"
+    mkdir -p $installdir
+fi
+echo "Installation directory is $installdir"
+
 
 exit
 # Desktop Environment [cli/lxde/mate]
