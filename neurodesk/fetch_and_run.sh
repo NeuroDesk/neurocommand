93d5358c4188a20d0192b6bc998c8501481b3493
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-02-05 12:53:54 +1000

    more debug output

--- neurodesk/fetch_and_run.sh
+++ neurodesk/fetch_and_run.sh
@@ -4,35 +4,35 @@
 # Example:
 #   fetch_and_run.sh itksnap 3.8.0 20200505 itksnap-wt
 
-echo "fetching containers:"
+echo "[DEBUG] fetch_and_run.sh: fetching containers:"
 
 source ~/.bashrc
 _script="$(readlink -f ${BASH_SOURCE[0]})" ## who am i? ##
 _base="$(dirname $_script)" ## Delete last component from $_script ##
-echo "Script name : $_script"
-echo "Current working dir : $PWD"
-echo "Script location path (dir) : $_base"
+echo "[DEBUG] fetch_and_run.sh: Script name : $_script"
+echo "[DEBUG] fetch_and_run.sh: Current working dir : $PWD"
+echo "[DEBUG] fetch_and_run.sh: Script location path (dir) : $_base"
 
 source ${_base}/configparser.sh ${_base}/config.ini
 source ${_base}/fetch_containers.sh $1 $2 $3
-echo "fetching containers done."
-echo "MOD_NAME: " $MOD_NAME
-echo "MOD_NAME: " $MOD_VERS
+echo "[DEBUG] fetch_and_run.sh: fetching containers done."
+echo "[DEBUG] fetch_and_run.sh: MOD_NAME: " $MOD_NAME
+echo "[DEBUG] fetch_and_run.sh: MOD_VERS: " $MOD_VERS
 
 
-echo "Module '${MOD_NAME}/${MOD_VERS}' is installed. Use the command 'module load ${MOD_NAME}/${MOD_VERS}' outside of this shell to use it."
+echo "[DEBUG] fetch_and_run.sh: Module '${MOD_NAME}/${MOD_VERS}' is installed. Use the command 'module load ${MOD_NAME}/${MOD_VERS}' outside of this shell to use it."
 
 # If no additional command -> Give user a shell in the image
 if [ $# -le 3 ]; then
     CONTAINER_FILE_NAME=${CONTAINER_PATH}/${IMG_NAME}/${IMG_NAME}.simg
-    echo "looking for ${CONTAINER_FILE_NAME}"
+    echo "[DEBUG] fetch_and_run.sh: looking for ${CONTAINER_FILE_NAME}"
     if [ -f "${CONTAINER_FILE_NAME}" ]; then
         cd 
-        echo "Attempting to launch container ${IMG_NAME}"
+        echo "[DEBUG] fetch_and_run.sh: Attempting to launch container ${IMG_NAME}"
         singularity exec ${CONTAINER_FILE_NAME} cat /README.md
         singularity shell ${CONTAINER_FILE_NAME}
         if [ $? -eq 0 ]; then
-            echo "Container ran OK"
+            echo "[DEBUG] fetch_and_run.sh: Container ran OK"
         else
             echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
             echo "the container ${CONTAINER_FILE_NAME} experienced an error. If you want to trigger a reinstall, run:"
@@ -54,7 +54,7 @@ if [ $# -le 3 ]; then
 fi
 
 # If additional command -> Run it
-echo "module load ${MOD_NAME}/${MOD_VERS}"
+echo "[DEBUG] fetch_and_run.sh: module load ${MOD_NAME}/${MOD_VERS}"
 module load ${MOD_NAME}/${MOD_VERS}
-echo "Running command '${@:4}'."
+echo "[DEBUG] fetch_and_run.sh: Running command '${@:4}'."
 ${@:4}
--- neurodesk/fetch_containers.sh
+++ neurodesk/fetch_containers.sh
@@ -12,6 +12,7 @@ MOD_VERS=$2
 MOD_DATE=$3
 
 IMG_NAME=${MOD_NAME}_${MOD_VERS}_${MOD_DATE}
+echo "[DEBUG] fetch_containers.sh: IMG_NAME=$IMG_NAME"
 
 _script="$(readlink -f ${BASH_SOURCE[0]})" ## who am i? ##
 _base="$(dirname $_script)" ## Delete last component from $_script ##
@@ -23,38 +24,39 @@ export PATH_PREFIX=${vnm_installdir}
 export CONTAINER_PATH=$PATH_PREFIX/containers
 export MODS_PATH=$CONTAINER_PATH/modules
 
-echo "CONTAINER_PATH: $CONTAINER_PATH"
-echo "MODS_PATH: $MODS_PATH"
+echo "[DEBUG] fetch_containers.sh: CONTAINER_PATH=$CONTAINER_PATH"
+echo "[DEBUG] fetch_containers.sh: MODS_PATH=$MODS_PATH"
 
-echo "trying to module use  ${MODS_PATH}"
+echo "[DEBUG] fetch_containers.sh: trying to module use  ${MODS_PATH}"
 if [ -f '/usr/share/module.sh' ]; then source /usr/share/module.sh; fi
 module use ${MODS_PATH}
 
 if [ ! -L `readlink -f $CONTAINER_PATH` ]; then
-    echo "creating `readlink -f $CONTAINER_PATH`"
+    echo "[DEBUG] fetch_containers.sh: creating `readlink -f $CONTAINER_PATH`"
     mkdir -p `readlink -f $CONTAINER_PATH` || ( echo "Something went wrong. " && exit )
 fi
 
 if [ ! -d `readlink -f $MODS_PATH` ]; then
-    echo "creating `readlink -f $MODS_PATH`"
+    echo "[DEBUG] fetch_containers.sh: creating `readlink -f $MODS_PATH`"
     mkdir -p `readlink -f $MODS_PATH` || ( echo "Something went wrong. " && exit )
 fi
 # Update application transparent-singularity with latest version
 cd ${CONTAINER_PATH}
 mkdir -p ${IMG_NAME}
-# Check if the module is there - if not this means we definetly need to install the container
+
+echo "[DEBUG] fetch_containers.sh: Check if the container is there - if not this means we definetly need to install the container"
 
 CONTAINER_FILE_NAME=${CONTAINER_PATH}/${IMG_NAME}/${IMG_NAME}.simg
 if [ -f "${CONTAINER_FILE_NAME}" ]; then
-    echo "found it. Container ${IMG_NAME} is there."
-    echo "now checking if container is fully downloaded and executable:"
+    echo "[DEBUG] fetch_containers.sh: found it. Container ${IMG_NAME} is there."
+    echo "[DEBUG] fetch_containers.sh: now checking if container is fully downloaded and executable:"
     qq=`which  singularity`
     if [[  ${#qq} -lt 1 ]]; then
-        echo "ERROR: This script requires singularity on your path. EXITING"
+        echo "[DEBUG] fetch_containers.sh: ERROR: This script requires singularity on your path. EXITING"
         exit 2
     fi
 
-    echo "testing if the container runs:"
+    echo "[DEBUG] fetch_containers.sh: testing if the container runs:"
     singularity exec ${CONTAINER_FILE_NAME} ls
     if [ $? -ne 0 ]; then
         echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
@@ -66,15 +68,15 @@ if [ -f "${CONTAINER_FILE_NAME}" ]; then
         [[ "$choice" == [Yy]* ]] && rm -rf ${CONTAINER_PATH}/${MOD_NAME}_${MOD_VERS}_* && rm -rf ${MODS_PATH}/${MOD_NAME}/${MOD_VERS}
         exit 2
     else 
-        echo "Container ${IMG_NAME} seems to be fully downloaded and executable."        
+        echo "[DEBUG] fetch_containers.sh: Container ${IMG_NAME} seems to be fully downloaded and executable."        
     fi
 else
-    echo "copying transparent singularity files from ${vnm_installdir} to ${CONTAINER_PATH}/${IMG_NAME} ..."
+    echo "[DEBUG] fetch_containers.sh: copying transparent singularity files from ${vnm_installdir} to ${CONTAINER_PATH}/${IMG_NAME} ..."
     cp ${vnm_installdir}/transparent-singularity/*.sh ${CONTAINER_PATH}/${IMG_NAME}/
     cp ${vnm_installdir}/transparent-singularity/ts_* ${CONTAINER_PATH}/${IMG_NAME}/
-    echo "changing directory to: ${CONTAINER_PATH}/${IMG_NAME}"
+    echo "[DEBUG] fetch_containers.sh: changing directory to: ${CONTAINER_PATH}/${IMG_NAME}"
     cd ${CONTAINER_PATH}/${IMG_NAME}
-    echo "executing run_transparent_singularity.sh --container ${IMG_NAME}.simg in $PWD"
+    echo "[DEBUG] fetch_containers.sh: executing run_transparent_singularity.sh --container ${IMG_NAME}.simg in $PWD"
     ${CONTAINER_PATH}/${IMG_NAME}/run_transparent_singularity.sh --container ${IMG_NAME}.simg
     # rm -rf .git* README.md run_transparent_singularity ts_*
 fi
