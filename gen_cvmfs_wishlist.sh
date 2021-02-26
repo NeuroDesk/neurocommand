33361d2b43fd4e019fda3690e2e0f3d410fe4124
Steffen Bollmann <s.bollmann@uq.edu.au>
2021-02-26 00:31:08 +0000

    log should not be commited

---
+++ gen_cvmfs_wishlist.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# set -e
+
+echo "building cvmfs wishlist"
+
+#creating logfile with available containers
+python3 neurodesk/write_log.py
+
+# remove empty lines
+sed -i '/^$/d' log.txt
+
+# remove square brackets
+sed -i 's/[][]//g' log.txt
+
+# remove spaces around
+sed -i -e 's/^[ \t]*//' -e 's/[ \t]*$//' log.txt
+
+# replace spaces with underscores
+sed -i 's/ /_/g' log.txt
+
+
+while IFS= read -r IMAGENAME_BUILDDATE
+do
+    IMAGENAME="$(cut -d'_' -f1,2 <<< ${IMAGENAME_BUILDDATE})"
+    BUILDDATE="$(cut -d'_' -f3 <<< ${IMAGENAME_BUILDDATE})"
+    echo "[DEBUG] IMAGENAME: $IMAGENAME"
+    echo "[DEBUG] BUILDDATE: $BUILDDATE"
+    docker://vnmd/$IMAGENAME:$BUILDDATE
+done < log.txt
