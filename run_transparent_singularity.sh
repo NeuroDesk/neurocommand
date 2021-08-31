80e3a7788458885170fec57a7b9b70b55a774f15
Github Action <actions@github.com>
2021-08-31 10:27:03 +0000

    Squashed 'neurodesk/transparent-singularity/' changes from e07d720..32a2b39
    32a2b39 sudo not needed for unpacking? - the errors I saw might have been caused by something else?
    
    git-subtree-dir: neurodesk/transparent-singularity
    git-subtree-split: 32a2b397343e7fd69d697c294822a4332d179de8

--- run_transparent_singularity.sh
+++ run_transparent_singularity.sh
@@ -211,7 +211,7 @@ chmod a+x deactivate_${container}.sh
 if [[ $unpack = "true" ]]
 then
    echo "unpacking singularity file to sandbox directory:"
-    sudo singularity build --sandbox temp $container
+    singularity build --sandbox temp $container
     rm -rf $container
     mv temp $container
 fi
