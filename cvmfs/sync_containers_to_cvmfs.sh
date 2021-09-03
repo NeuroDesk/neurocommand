0f4b3581341198b72a717b6c3545363c8f4ad043
stebo85 <stebo85@web.de>
2021-09-03 14:49:02 +1000

    added sync script

---
+++ cvmfs/sync_containers_to_cvmfs.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+# set -e
+
+#This script runs on the CVMFS STRATUM 0 server
+
+#sudo vi /etc/cron.d/sync_containers_to_cvmfs
+#*/5* * * * ec2-user cd ~ && bash /home/ec2-user/neurodesk/cvmfs/sync_containers_to_cvmfs.sh
+
+cd ~/neurodesk/
+
+# change to ssh
+# git remote set-url origin git@github.com:NeuroDesk/neurodesk.git
+
+
+# update application list:
+git pull
+cd cvmfs
+./gen_cvmfs_wishlist.sh
+git commit -am 'auto commit after CVMFS'
+git push
+
+# check if there is enough free space - otherwise don't do anything:
+FREE=`df -k --output=avail /storage | tail -n1`
+if [[ $FREE -lt 400000000 ]]; then               # 400GB = 
+    echo "There is not enough free disk space!"
+    exit 1
+fi;
+
+# download and unpack containers on cvmfs
+# curl -s https://raw.githubusercontent.com/NeuroDesk/neurodesk/master/cvmfs/log.txt
+# export IMAGENAME_BUILDDATE=fsl_6.0.3_20200905
+# export IMAGENAME_BUILDDATE=mrtrix3_3.0.1_20200908
+# export IMAGENAME_BUILDDATE=spm12_r7219_20201120
+# export LINE='fsl_6.0.4_20210105 categories:functional imaging,structural imaging,diffusion imaging,image segmentation,image registration,'
+
+Field_Separator=$IFS
+echo $Field_Separator
+
+
+while IFS= read -r LINE
+do
+    echo "LINE: $LINE"
+    IMAGENAME_BUILDDATE="$(cut -d' ' -f1 <<< ${LINE})"
+    echo "IMAGENAME_BUILDDATE: $IMAGENAME_BUILDDATE"
+
+    CATEGORIES=`echo $LINE | awk -F"categories:" '{print $2}'`
+    echo "CATEGORIES: $CATEGORIES"
+
+    echo "check if $IMAGENAME_BUILDDATE is in module files:"
+    TOOLNAME="$(cut -d'_' -f1 <<< ${IMAGENAME_BUILDDATE})"
+    TOOLVERSION="$(cut -d'_' -f2 <<< ${IMAGENAME_BUILDDATE})"
+    BUILDDATE="$(cut -d'_' -f3 <<< ${IMAGENAME_BUILDDATE})"
+    echo "[DEBUG] TOOLNAME: $TOOLNAME"
+    echo "[DEBUG] TOOLVERSION: $TOOLVERSION"
+    echo "[DEBUG] BUILDDATE: $BUILDDATE"
+
+    echo "check if $IMAGENAME_BUILDDATE is already on cvmfs:"
+    if [[ -d "/cvmfs/neurodesk.ardc.edu.au/containers/$IMAGENAME_BUILDDATE" ]]
+    then
+        echo "$IMAGENAME_BUILDDATE exists on cvmfs"
+    else
+        echo "$IMAGENAME_BUILDDATE is not yet on cvmfs. Downloading now!"
+        cvmfs_server transaction neurodesk.ardc.edu.au
+
+        cd /cvmfs/neurodesk.ardc.edu.au/containers/
+        git clone https://github.com/NeuroDesk/transparent-singularity $IMAGENAME_BUILDDATE
+        cd $IMAGENAME_BUILDDATE
+        export SINGULARITY_BINDPATH=/cvmfs
+        ./run_transparent_singularity.sh $IMAGENAME_BUILDDATE --unpack true
+
+        cd && cvmfs_server publish -m "added $IMAGENAME_BUILDDATE" neurodesk.ardc.edu.au
+    fi
+
+    # set internal field separator for the string list
+    echo $CATEGORIES
+    IFS=','
+    for CATEGORY in $CATEGORIES;
+    do
+        echo $CATEGORY
+        CATEGORY="${CATEGORY// /_}"
+
+        if [[ -a "/cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/$CATEGORY/$TOOLNAME/$TOOLVERSION" ]]
+        then
+            echo "$IMAGENAME_BUILDDATE exists in module $CATEGORY"
+        else
+            cvmfs_server transaction neurodesk.ardc.edu.au
+            mkdir -p /cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/$CATEGORY/$TOOLNAME/
+            cp /cvmfs/neurodesk.ardc.edu.au/containers/modules/$TOOLNAME/$TOOLVERSION /cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/$CATEGORY/$TOOLNAME/$TOOLVERSION
+            cd && cvmfs_server publish -m "added modules for $IMAGENAME_BUILDDATE" neurodesk.ardc.edu.au
+            if  [[ -f /cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/$CATEGORY/$TOOLNAME/$TOOLVERSION ]]; then
+                echo "module file $CATEGORY/$TOOLNAME/$TOOLVERSION written. This worked!"
+            else
+                echo "Something went wrong: cp /cvmfs/neurodesk.ardc.edu.au/containers/modules/$TOOLNAME/$TOOLVERSION /cvmfs/neurodesk.ardc.edu.au/neurodesk-modules/$CATEGORY/$TOOLNAME/$TOOLVERSION"
+                exit 2
+            fi
+        fi
+    done
+    
+    IFS=$Field_Separator
+
+done < /home/ec2-user/neurodesk/cvmfs/log.txt
+
+
+# check if catalog is OK
+cvmfs_server list-catalogs -e
+
+# export DUCC_DOCKER_REGISTRY_PASS=0xlu08JW33gz
+# cvmfs_ducc convert recipe_neurodesk_auto.yaml
+
+
+# garbage collection
+# sudo cvmfs_server gc neurodesk.ardc.edu.au
+cvmfs_server tag -l
+# cvmfs_server tag -r spm12_r7219_20201120
