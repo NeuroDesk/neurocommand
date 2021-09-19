9237231a6ac6c85b6d55282354202e1f9c3013fb
stebo85 <stebo85@web.de>
2021-09-19 20:59:12 +1000

    add file so it can be committed

--- .github/workflows/upload_containers_simg.sh
+++ .github/workflows/upload_containers_simg.sh
@@ -83,5 +83,6 @@ done < log.txt
 
 #once everything is uploaded successfully move log file to cvmfs folder, so cvmfs can start downloading the containers:
 mv log.txt cvmfs
+git add cvmfs/log.txt
 # git commit -am 'commit container log file after successfull run'
 # this file will be committed via uses: stefanzweifel/git-auto-commit-action@v4
