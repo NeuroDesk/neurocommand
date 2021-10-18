5f30017a11df7ace94837c3d797508d91d91b1e3
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-10-18 15:43:18 +1000

    change how lines get parsed

--- .github/workflows/upload_containers_simg.sh
+++ .github/workflows/upload_containers_simg.sh
@@ -61,27 +61,27 @@ do
     else
         # check if there is enough free disk space on the runner:
         FREE=`df -k --output=avail "$PWD" | tail -n1`   # df -k not df -h
-        # echo "[DEBUG] This runner has ${FREE} free disk space"
+        echo "[DEBUG] This runner has ${FREE} free disk space"
         if [[ $FREE -lt 30485760 ]]; then               # 30G = 10*1024*1024k
             echo "[DEBUG] This runner has not enough free disk space .. cleaning up!"
             bash .github/workflows/free-up-space.sh > /dev/null 2>&1
         fi;
 
-        # echo "[DEBUG] singularity building docker://vnmd/$IMAGENAME:$BUILDDATE"
+        echo "[DEBUG] singularity building docker://vnmd/$IMAGENAME:$BUILDDATE"
         singularity build "$IMAGE_HOME/${IMAGENAME_BUILDDATE}.simg"  docker://vnmd/$IMAGENAME:$BUILDDATE > /dev/null 2>&1
 
-        # echo "[DEBUG] Attempting upload to Oracle ..."
+        echo "[DEBUG] Attempting upload to Oracle ..."
         curl -X PUT -u ${ORACLE_USER} --upload-file $IMAGE_HOME/${IMAGENAME_BUILDDATE}.simg $ORACLE_NEURODESK_BUCKET > /dev/null 2>&1
 
-        # if curl --output /dev/null --silent --head --fail "https://objectstorage.us-ashburn-1.oraclecloud.com/n/sd63xuke79z3/b/neurodesk/o/${IMAGENAME_BUILDDATE}.simg"; then
-        #     # echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg was freshly build and exists now :)"
-        #     # echo "[DEBUG] PROCEEDING TO NEXT LINE"
-        # else
-        #     echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg does not exist yet. Something is WRONG"
-        #     exit 2
-        # fi
+        if curl --output /dev/null --silent --head --fail "https://objectstorage.us-ashburn-1.oraclecloud.com/n/sd63xuke79z3/b/neurodesk/o/${IMAGENAME_BUILDDATE}.simg"; then
+            echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg was freshly build and exists now :)"
+            echo "[DEBUG] PROCEEDING TO NEXT LINE"
+        else
+            echo "[DEBUG] ${IMAGENAME_BUILDDATE}.simg does not exist yet. Something is WRONG"
+            exit 2
+        fi
     fi 
-done < log.txt
+done <<< $(cat log.txt )
 
 #once everything is uploaded successfully move log file to cvmfs folder, so cvmfs can start downloading the containers:
 echo "[Debug] mv logfile to cvmfs directory"
