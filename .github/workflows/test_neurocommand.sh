edec82e5d17f24c2cfad00296d6da8ca3d6579ec
Steffen Bollmann <stebo85@users.noreply.github.com>
2023-01-12 13:50:36 +1000

    update runner to 20.04

--- .github/workflows/test_neurocommand.sh
+++ .github/workflows/test_neurocommand.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
-# set -e
+set -e
 
 #setup singularity 2.6.1 from neurodebian
-wget -O- http://neuro.debian.net/lists/bionic.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
-sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+# This is for 18.04 (deprecated)
+# wget -O- http://neuro.debian.net/lists/bionic.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+# sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+
+# This is for 20.04 (singularity-container is not build for later versions in neuro debian project yet)
+wget -O- http://neuro.debian.net/lists/focal.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+sudo apt-key adv --recv-keys --keyserver hkps://keyserver.ubuntu.com 0xA5D32F012649A5A9
+
 sudo apt-get update
 sudo apt-get install singularity-container 
 
