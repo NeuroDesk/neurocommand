7dbcdd7d98affe6315a0aaf3ab4b40e5787fa8d9
Steffen Bollmann <stebo85@users.noreply.github.com>
2023-02-06 10:42:53 +1000

    made container list more userfriendly
    problems of the old list:
    - had duplicate entries
    - the shell command produced wasn't very readable
    
    new list: 
    - shows categories as well
    - is based on log.txt from cvmfs
    - repeats instruction at the end

--- containers.sh
+++ containers.sh
@@ -1,38 +1,49 @@
-#!/bin/bash
-
-_script="$(readlink -f ${BASH_SOURCE[0]})" ## who am i? ##
-_base="$(dirname $_script)" ## Delete last component from $_script ##
-source neurodesk/configparser.sh ${_base}/config.ini
-
-install_all_containers="false"
-
-if [ "$1" != "" ]; then
-    echo "Installing all containers"
-    install_all_containers="true"
-fi
-
-echo "------------------------------------"
-echo "to install ALL containers, run:"
-echo "bash containers.sh --all"
-echo "------------------------------------"
-echo "to install individual containers, run:"
-for appsh in ${neurodesk_installdir}/bin/*.sh; do
-    appfetch=$(sed -n 's/fetch_and_run.sh/fetch_containers.sh/p' $appsh)
-    echo $appfetch
-    if [ "$install_all_containers" = "true" ]; then
-         eval $appfetch
-        err=$?
-        if [ $err -eq 0 ] ; then
-            echo "Container successfully installed"
+#!/bin/bash         
+                    
+_script="$(readlink -f ${BASH_SOURCE[0]})" ## who am i? ##                            
+_base="$(dirname $_script)" ## Delete last component from $_script ##                 
+source neurodesk/configparser.sh ${_base}/config.ini                                  
+                    
+install_all_containers="false"                                                        
+                    
+if [ "$1" != "" ]; then                                                               
+    echo "Installing all containers"                                                  
+    install_all_containers="true"                                                     
+fi                  
+                    
+echo "------------------------------------"                                           
+echo "to install ALL containers, run:"                                                
+echo "bash containers.sh --all"                                                       
+echo "------------------------------------"                                           
+echo "to install individual containers, run:"                                         
+while read appsh; do
+------              
+      echo $appsh run:                                                                
+      arrayIn=(${appsh//_/ })                                                         
+      appfetch="./local/fetch_containers.sh ${arrayIn[0]} ${arrayIn[1]} ${arrayIn[2]}"
+      echo $appfetch                                                                                                                                                
+      echo ""       
+                    
+ if [ "$install_all_containers" = "true" ]; then                                      
+         eval $appfetch                                                               
+        err=$?      
+        if [ $err -eq 0 ] ; then                                                      
+            echo "Container successfully installed"                                   
             echo "-------------------------------------------------------------------------------------"
-            date
-        else
-            echo "======================================="
-            echo "!!!!!!! Container install failed !!!!!!"
-            echo "======================================="
-            date
-            exit
-        fi
-        
-    fi
-done
+            date    
+        else        
+            echo "======================================="                            
+            echo "!!!!!!! Container install failed !!!!!!"                            
+            echo "======================================="                            
+            date    
+            exit    
+        fi          
+--------            
+    fi              
+done < cvmfs/log.txt
+                    
+echo "------------------------------------"                                           
+echo "to install ALL containers, run:"                                                
+echo "bash containers.sh --all"                                                       
+echo "------------------------------------"                                           
+echo "to install individual containers, run the above listed commands for each application"
