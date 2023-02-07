06a137160fd52ec7615bfa7841fc95a4a2bafe79
aswinnarayanan <aswinn89@gmail.com>
2023-02-07 20:57:31 +1000

    Add formatted msgs and dividers

--- containers.sh
+++ containers.sh
@@ -1,49 +1,65 @@
-#!/bin/bash         
-                    
-_script="$(readlink -f ${BASH_SOURCE[0]})" ## who am i? ##                            
-_base="$(dirname $_script)" ## Delete last component from $_script ##                 
-source neurodesk/configparser.sh ${_base}/config.ini                                  
-                    
-install_all_containers="false"                                                        
-                    
-if [ "$1" != "" ]; then                                                               
-    echo "Installing all containers"                                                  
-    install_all_containers="true"                                                     
-fi                  
-                    
-echo "------------------------------------"                                           
-echo "to install ALL containers, run:"                                                
-echo "bash containers.sh --all"                                                       
-echo "------------------------------------"                                           
-echo "to install individual containers, run:"                                         
+#!/bin/bash
+
+_script="$(readlink -f ${BASH_SOURCE[0]})" ## who am i? ##
+_base="$(dirname $_script)" ## Delete last component from $_script ##
+source neurodesk/configparser.sh ${_base}/config.ini
+
+install_all_containers="false"
+
+if [ "$1" != "" ]; then
+    echo "Installing all containers"
+    install_all_containers="true"
+fi
+
+echo "------------------------------------"
+echo "to install ALL containers, run:"
+echo "bash containers.sh --all"
+echo "------------------------------------"
+echo "to install individual containers, run:"
+echo
 while read appsh; do
-------              
-      echo $appsh run:                                                                
-      arrayIn=(${appsh//_/ })                                                         
+      arrayIn=(${appsh//_/ })
+      appcat=$(echo ${arrayIn[3]} | cut -c 12-)
+      apphead="| ${arrayIn[0]} | ${arrayIn[1]} | ${arrayIn[2]} | ${appcat} | Run:"
       appfetch="./local/fetch_containers.sh ${arrayIn[0]} ${arrayIn[1]} ${arrayIn[2]}"
-      echo $appfetch                                                                                                                                                
-      echo ""       
-                    
- if [ "$install_all_containers" = "true" ]; then                                      
-         eval $appfetch                                                               
-        err=$?      
-        if [ $err -eq 0 ] ; then                                                      
-            echo "Container successfully installed"                                   
-            echo "-------------------------------------------------------------------------------------"
-            date    
-        else        
-            echo "======================================="                            
-            echo "!!!!!!! Container install failed !!!!!!"                            
-            echo "======================================="                            
-            date    
-            exit    
-        fi          
---------            
-    fi              
+
+    eval $(echo printf '"%.0s-"' {1..${#apphead}})
+    echo
+    echo $apphead
+    eval $(echo printf '"%.0s-"' {1..${#apphead}})
+    echo
+    echo $appfetch
+    echo
+
+ if [ "$install_all_containers" = "true" ]; then
+        eval $appfetch
+        err=$?
+        if [ $err -eq 0 ] ; then
+            installmsg="| SUCCESS | ${arrayIn[0]} ${arrayIn[1]} ${arrayIn[2]} | $(date)"
+            eval $(echo printf '"%.0s-"' {1..${#installmsg}})
+            echo
+            echo $installmsg
+            eval $(echo printf '"%.0s-"' {1..${#installmsg}})
+            echo
+            echo
+            echo
+        else
+            installmsg="| FAILED | ${arrayIn[0]} ${arrayIn[1]} ${arrayIn[2]} | $(date)"
+            eval $(echo printf '"%.0s-"' {1..${#installmsg}})
+            echo
+            echo $installmsg
+            eval $(echo printf '"%.0s-"' {1..${#installmsg}})
+            echo
+            echo
+            echo "Existing due to install error(s) ..."
+            echo
+            exit
+        fi
+    fi
 done < cvmfs/log.txt
-                    
-echo "------------------------------------"                                           
-echo "to install ALL containers, run:"                                                
-echo "bash containers.sh --all"                                                       
-echo "------------------------------------"                                           
+
+echo "------------------------------------"
+echo "to install ALL containers, run:"
+echo "bash containers.sh --all"
+echo "------------------------------------"
 echo "to install individual containers, run the above listed commands for each application"
