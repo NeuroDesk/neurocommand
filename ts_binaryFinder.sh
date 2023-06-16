6324589d8c0290a81a6f38ea9b3f772210696780
Github Action <actions@github.com>
2023-06-16 03:57:50 +0000

    Squashed 'neurodesk/transparent-singularity/' changes from 9bb74c9..c4cb0d6
    c4cb0d6 exclude more binaries
    
    git-subtree-dir: neurodesk/transparent-singularity
    git-subtree-split: c4cb0d6b386ed6d40d79bc4f75a5e8b2c3990b26

--- ts_binaryFinder.sh
+++ ts_binaryFinder.sh
@@ -11,10 +11,13 @@ for i in $DEPLOY_BINS; \
 #    export DEPLOY_ENV_SPMMCRCMD="BASEPATH/opt/spm12/run_spm12.sh BASEPATH/opt/mcr/v97/ script"
 # fi
 
+# IFS=':'; \
+# for i in $PATH; \
+#    do find "$i" -maxdepth 1 -executable -type f -exec basename {} \;; done > system_commands_raw.txt
 
 # Remove system applications from commands.txt, because they cause problems:
 getListOfSystemCommandsToBeDeleted() {
-  printf '%s\n' conda bash cat chmod cp cut date echo env find grep head ln ls mkdir mv pwd rm sed sort tail touch tr uname uniq wc
+  printf '%s\n' `cat ts_binaryFinderExcludes.txt`
 }
 
 sed -E 's/\<('"$(tr '\n' '|' < <(getListOfSystemCommandsToBeDeleted) )"')\>//gI' < commands_raw.txt > commands.txt
