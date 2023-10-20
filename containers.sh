2d84649ed0a93a28dad7b89e3de628d2854ff29c
Marcel Zwiers <mpzwiers@gmail.com>
2023-10-21 01:44:49 +0200

    Revert the `exit 0` fix

--- containers.sh
+++ containers.sh
@@ -14,7 +14,7 @@ if [[ -z "$1" ]] || [ "$1" = "-h" ] || [ "$1" = "--help" ] ; then
     echo "  $0 diffusion"
     echo "  $0 --bidscoin"
     echo
-    exit 0
+    exit 1
 fi
 
 _script="$(readlink -f ${BASH_SOURCE[0]})" ## who am i? ##
