43c192007a29986bf94658b99f032b82cecee1f5
Steffen Bollmann <stebo85@users.noreply.github.com>
2023-12-14 20:34:53 +1000

    disable syncs for now

--- cvmfs/sync_containers_to_cvmfs.sh
+++ cvmfs/sync_containers_to_cvmfs.sh
@@ -19,11 +19,11 @@ else
     rm ~/cronjob.log
 fi
 
-echo "Syncing object storages:"
-export RCLONE_VERBOSE=2
-rclone sync oracle-2021-us-bucket:/neurodesk/temporary-builds oracle-2021-sydney-bucket:/neurodesk/temporary-builds
-rclone sync oracle-2021-us-bucket:/neurodesk oracle-2021-sydney-bucket:/neurodesk
-rclone copy oracle-2021-sydney-bucket:/neurodesk nectar:/neurodesk/
+# echo "Syncing object storages:"
+# export RCLONE_VERBOSE=2
+# rclone sync oracle-2021-us-bucket:/neurodesk/temporary-builds oracle-2021-sydney-bucket:/neurodesk/temporary-builds
+# rclone sync oracle-2021-us-bucket:/neurodesk oracle-2021-sydney-bucket:/neurodesk
+# rclone copy oracle-2021-sydney-bucket:/neurodesk nectar:/neurodesk/
 
 cd ~/neurocommand/
 
