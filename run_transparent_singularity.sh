8e3691459d053034b99a6e5f4565d639d51b200a
Github Action <actions@github.com>
2025-02-11 01:00:58 +0000

    Squashed 'neurodesk/transparent-singularity/' changes from 1ed24f8..8b22053
    8b22053 needs quotes around path
    
    git-subtree-dir: neurodesk/transparent-singularity
    git-subtree-split: 8b220533f899bdf081f7b05cc3abff0a77294b6f

--- run_transparent_singularity.sh
+++ run_transparent_singularity.sh
@@ -298,7 +298,7 @@ cat README.md >> ${modulePath}/${moduleName}.lua
 echo "]])" >> ${modulePath}/${moduleName}.lua
 
 echo "whatis(\"${container}\")" >> ${modulePath}/${moduleName}.lua
-echo "prepend_path(\"PATH\", ${_base})" >> ${modulePath}/${moduleName}.lua
+echo "prepend_path(\"PATH\", \"${_base}\")" >> ${modulePath}/${moduleName}.lua
 
 echo "create environment variables for module file"
 while read envvariable; do \
@@ -315,7 +315,7 @@ while read envvariable; do \
    variableName=${completeVariableName#*DEPLOY_ENV_}
    # echo $variableName
 
-   echo "setenv(\"${variableName}\", ${value_with_basepath})" >> ${modulePath}/${moduleName}.lua
+   echo "setenv(\"${variableName}\", \"${value_with_basepath}\")" >> ${modulePath}/${moduleName}.lua
 done < $_base/env.txt
 
 #check if there is a manual module file for this container and add it to the end
