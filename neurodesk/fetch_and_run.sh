9fd5c84a447c513b40c6da71b2208f3c92adb809
Steffen Bollmann <stebo85@users.noreply.github.com>
2025-02-27 15:18:32 +1000

    add more error handling

--- neurodesk/fetch_and_run.sh
+++ neurodesk/fetch_and_run.sh
@@ -5,8 +5,8 @@
 #   fetch_and_run.sh itksnap 3.8.0 20200505 itksnap-wt
 
 # source ~/.bashrc
-_script="$(readlink -f ${BASH_SOURCE[0]})" ## who am i? ##
-_base="$(dirname $_script)" ## Delete last component from $_script ##
+_script="$(readlink -f "${BASH_SOURCE[0]}")" ## who am i? ##
+_base="$(dirname "$_script")" ## Delete last component from $_script ##
 echo "[DEBUG] fetch_and_run.sh: Script name : $_script"
 echo "[DEBUG] fetch_and_run.sh: Current working dir : $PWD"
 echo "[DEBUG] fetch_and_run.sh: Script location path (dir) : $_base"
@@ -19,7 +19,8 @@ then
       `cat /etc/bash.bashrc | grep SINGULARITY_BINDPATH`
 fi
 
-source ${_base}/configparser.sh ${_base}/config.ini
+# shellcheck disable=SC1091
+source "${_base}"/configparser.sh "${_base}"/config.ini
 
 MOD_NAME=$1
 MOD_VERS=$2
@@ -44,13 +45,15 @@ if [ -z "$CVMFS_DISABLE" ]; then
         module use ${MODS_PATH}
 else
         echo "Not using CVMFS! Downloading containers fully!"
-        source ${_base}/fetch_containers.sh $1 $2 $3
+        # shellcheck disable=SC1091
+        source "${_base}"/fetch_containers.sh "$1" "$2" "$3"
+        CONTAINER_PATH="${_base}"/containers
 fi
 
 
 echo "[DEBUG] fetch_and_run.sh: fetching containers done."
-echo "[DEBUG] fetch_and_run.sh: MOD_NAME: " $MOD_NAME
-echo "[DEBUG] fetch_and_run.sh: MOD_VERS: " $MOD_VERS
+echo "[DEBUG] fetch_and_run.sh: MOD_NAME: " "$MOD_NAME"
+echo "[DEBUG] fetch_and_run.sh: MOD_VERS: " "$MOD_VERS"
 
 
 echo "[DEBUG] fetch_and_run.sh: Module '${MOD_NAME}/${MOD_VERS}' is installed. Use the command 'module load ${MOD_NAME}/${MOD_VERS}' outside of this shell to use it."
@@ -62,23 +65,43 @@ if [ $# -le 3 ]; then
     if [ -e "${CONTAINER_FILE_NAME}" ]; then
         cd 
         echo "[DEBUG] fetch_and_run.sh: Module loading the container to set environment variables."
-        module load ${MOD_NAME}/${MOD_VERS}
+        module load "${MOD_NAME}"/"${MOD_VERS}"
         echo "[DEBUG] fetch_and_run.sh: Attempting to launch container ${IMG_NAME}"
         
         export SINGULARITYENV_PS1="${MOD_NAME}-${MOD_VERS}:\w$ "
-        singularity --silent exec  ${neurodesk_singularity_opts} ${CONTAINER_FILE_NAME} cat /README.md
-        singularity --silent shell  ${neurodesk_singularity_opts} ${CONTAINER_FILE_NAME}
+        # shellcheck disable=SC2154
+        singularity --silent exec  "${neurodesk_singularity_opts}" "${CONTAINER_FILE_NAME}" cat /README.md
+        singularity --silent shell  "${neurodesk_singularity_opts}" "${CONTAINER_FILE_NAME}"
         if [ $? -eq 0 ]; then
             echo "[DEBUG] fetch_and_run.sh: Container ran OK"
         else
             echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-            echo "the container ${CONTAINER_FILE_NAME} experienced an error."
+            echo "the container ${CONTAINER_FILE_NAME} experienced an error when starting. This could be a problem with your firewall if it uses deep packet inspection. Please ask your IT if they do this and what they are blocking."
             echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+            
+            read -n 1 -s -r -p "Press any key to continue..."
+            
+            echo "downloading the complete container as a workaround ..."
+            # shellcheck disable=SC1091
+            source "${_base}"/fetch_containers.sh "$1" "$2" "$3"
+            CONTAINER_PATH="${_base}"/containers
+            CONTAINER_FILE_NAME=${CONTAINER_PATH}/${IMG_NAME}/${IMG_NAME}.simg
+            singularity --silent exec  "${neurodesk_singularity_opts}" "${CONTAINER_FILE_NAME}" cat /README.md
+            singularity --silent shell  "${neurodesk_singularity_opts}" "${CONTAINER_FILE_NAME}"
+            if [ $? -eq 0 ]; then
+                echo "[DEBUG] fetch_and_run.sh: Container ran OK"
+            else
+                echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+                echo "the container ${CONTAINER_FILE_NAME} doesn't exist. There is something wrong with the container download. Please ask for help here with the output of this window: https://github.com/orgs/NeuroDesk/discussions "
+                echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+                read -n 1 -s -r -p "Press any key to continue..."
+            fi
         fi
     else 
         echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        echo "the container ${CONTAINER_FILE_NAME} experienced an error."
+        echo "the container ${CONTAINER_FILE_NAME} doesn't exist. There is something wrong with the container download or CVMFS. Please ask for help here with the output of this window: https://github.com/orgs/NeuroDesk/discussions "
         echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        read -n 1 -s -r -p "Press any key to continue..."
     fi
 fi
 
