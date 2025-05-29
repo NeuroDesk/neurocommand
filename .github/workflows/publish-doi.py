6293320e03a688f596e3055c5b7bb2c86ab147c7
iishiishii <love_sweet_98@yahoo.com.vn>
2025-05-29 16:05:53 +1000

    Refactor license retrieval and update upload function to handle license metadata

--- .github/workflows/publish-doi.py
+++ .github/workflows/publish-doi.py
@@ -8,7 +8,6 @@ def get_license(container_name, gh_token):
     """
     Get the license from copyright field in YAML file in the container.
     """
-    recipe_name = container_name.split("/")[-1]
     # Get yaml recipe using github API
     headers = {
         "Accept": "application/vnd.github+json",
@@ -16,46 +15,47 @@ def get_license(container_name, gh_token):
         "X-GitHub-Api-Version": "2022-11-28",
     }
     # Get the recipe name from the container name
-    recipe_name = container_name.split("/")[-1]
-    url = f" https://api.github.com/repos/NeuroDesk/neurocontainers/contents/recipes/{recipe_name}/build.yaml"
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        # Get the license from the recipe
-        download_url = response.json()["download_url"]
+    recipe_name = container_name.split("_")[0]
+    url = f" https://api.github.com/repos/iishiishii/neurocontainers/contents/recipes/{recipe_name}/build.yaml"
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        download_url = response.json().get("download_url")
+        if not download_url:
+            print("No download_url found in GitHub API response")
+            return ""
         download_url_response = requests.get(download_url)
         download_url_response.raise_for_status()
         content = download_url_response.content.decode("utf-8")
         tinyrange_config = yaml.safe_load(content)
-        # print("Tinyrange config", tinyrange_config['copyright'])
-        copyrights = tinyrange_config.get('copyright', {})
+        copyrights = tinyrange_config.get('copyright')
         print("Copyright field", copyrights)
-        if not copyrights:
+        if not copyrights or not isinstance(copyrights, list):
             print("No copyright field found in the recipe")
-            return None, None
-        for copyright in copyrights:
-            license = copyright.get('license', None)
-            license_url = copyright.get('url', None)
-        return license, license_url
-    else:
-        print("Failed to get recipe", response.status_code, response.text)
+            return ""
+        # Return the first license found in the copyright field
+        # for copyright_entry in copyrights:
+        license = copyrights[0].get('license')
+        if license:
+            license_url = copyrights[0].get('url')
+            return {
+                    'id': license.lower(),
+                    'title': license,
+                    'url': license_url
+                    }
+        else:
+            license = copyrights[0].get('name')
+            license_url = copyrights[0].get('url')
+            return {
+                    'id': license.lower(),
+                    'title': license,
+                    'url': license_url
+                    }
+    except Exception as e:
+        print(f"Failed to get recipe or parse license: {e}")
         return ""
 
-def get_license_id(license_name):
-    # Get the license ID from Zenodo
-    headers = {"Content-Type": "application/json"}
-    response = requests.get('https://sandbox.zenodo.org/api/licenses/',
-                            headers=headers)
-    
-    if response.status_code == 200:
-        licenses = response.json()['hits']['hits']
-        for license in licenses:
-            if license['id'].lower() == license_name.lower():
-                return license['id']
-    else:
-        print("Failed to get licenses", response.status_code, response.text)
-    return None, None
-
-def upload_container(container_url, container_name, token, license, license_url):
+def upload_container(container_url, container_name, token, license):
     headers = {"Content-Type": "application/json"}
     params = {'access_token': token}
 
@@ -67,10 +67,25 @@ def upload_container(container_url, container_name, token, license, license_url)
     deposition_id = r.json()['id']
     bucket_url = r.json()["links"]["bucket"]
 
-    # Upload the simg container to bucket in the created deposition
-    # The target URL is a combination of the bucket link with the desired filename
-    # seperated by a slash.
-    # print("Uploading container to Zenodo...", container_url)
+    print("licenses", license)
+    # Update the metadata
+    data = {
+        'metadata': {
+            'title': container_name,
+            'upload_type': 'software',
+            'description': container_name,
+            'creators': [{
+                'name': 'Neurodesk',
+                'affiliation': 'University of Queensland'
+            }]
+        }
+    }
+    if license:
+        data['metadata']['license'] = license
+        print("Updating metadata", data)
+        r = requests.put('https://sandbox.zenodo.org/api/deposit/depositions/%s' % deposition_id,
+                        params=params, data=json.dumps(data),
+                        headers=headers)
     
     with requests.get(container_url, stream=True) as response:
         response.raise_for_status()  # Ensure the request was successful
@@ -79,26 +94,12 @@ def upload_container(container_url, container_name, token, license, license_url)
             data=response.content,  # Stream the file directly
             params=params,
         )
-
-    # Update the metadata
-    data = {
-        'metadata': {
-            'title': container_name,
-            'upload_type': 'software',
-            'description': container_name,
-            'license': license,
-            'creators': [{'name': 'Neurodesk',
-                        'affiliation': 'University of Queensland'}]
-        }
-    }
-    r = requests.put('https://sandbox.zenodo.org/api/deposit/depositions/%s' % deposition_id,
-                    params=params, data=json.dumps(data),
-                    headers=headers)
+    
 
     # Publish the deposition
     r = requests.post('https://sandbox.zenodo.org/api/deposit/depositions/%s/actions/publish' % deposition_id,
                       params=params )
-    # print("Publish", r.json())
+    print("Publish", r.json())
 
     # Get the DOI from the deposition
     r = requests.get('https://sandbox.zenodo.org/api/deposit/depositions/%s' % deposition_id,
@@ -115,9 +116,9 @@ if __name__ == '__main__':
     parser.add_argument("--container_filepath", type=str, required=True, help="Container file to upload to Zenodo")
     parser.add_argument("--container_name", type=str, required=True, help="Container name")
     parser.add_argument("--token", type=str, required=True, help="Zenodo token")
-    
+    parser.add_argument("--gh_token", type=str, required=True, help="GitHub token to access the recipe")
     args = parser.parse_args()
 
-    license, license_url = get_license(args.container_name, args.token)
-    doi_url = upload_container(args.container_filepath, args.container_name, args.token, license, license_url)
+    license = get_license(args.container_name, args.gh_token)
+    doi_url = upload_container(args.container_filepath, args.container_name, args.token, license)
     print(doi_url)
